3cc960834fcc0b5d7f21a29110a1584251409208
iLabUTK <ilab.utk@gmail.com>
2020-02-10 11:10:50 -0500

    new quasar added

---
+++ src/iLabWeb/src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="statics/iLab.png">
+    <q-card
+      class="my-card text-orange q-col-11"
+    >
+      <q-card-section class="q-pt-none">
+        <div style="text-align: center">
+          <q-btn to="./HITS/tberg/">Tom's Project</q-btn>
+        </div>
+        <div class="text-grey-7" style="text-align: center">
+          More to come ...
+          </div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+</style>
